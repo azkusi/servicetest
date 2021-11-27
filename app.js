f44dce0ff59680b661be896dfe9c7d590ddabbfa
azkusi <azkusi1@gmail.com>
2021-11-27 08:04:41 +0000

    show store content - title and description

--- app.js
+++ app.js
@@ -4,35 +4,104 @@ const port =  process.env.PORT || 5000;
 const host = '127.0.0.1'
 const path = require('path');
 const socket = require("socket.io");
+const axios = require('axios');
 
 var INChostname;
 var storename;
 
+var store_front_content;
+var booking_page_content;
+var account_details;
+
+// app.use('*', function(req, res, next){
+//   //myservvio.com allowed
+//   //wepaint.servvio.com allowed
+//   //we.paint.servvio.com not allowed
+// // if(req.hostname.match(/\./g).length > 2){
+// //   //send error page
+//     // res.send(static error page)
+// // }
+// // else if(req.hostname == 'myservvio.com'){
+// //   //send error page
+// // }
+// //else continue i.e. send the react static or dynamic page
+//   next()
+// })
 
 app.use('/', function(req, res, next){
-  //myservvio.com allowed
-  //wepaint.servvio.com allowed
-  //we.paint.servvio.com not allowed
-// if(req.hostname.match(/\./g).length > 2){
-//   //send error page
-// }
-// else if(req.hostname == 'myservvio.com'){
-//   //send error page
-// }
-//else continue i.e. send the react static or dynamic page
 
   INChostname = req.hostname
   storename = req.hostname.replace('.localhost', '')
 
   console.log(`hello here works in middleware req.data: ${req}, hostname: ${req.hostname}, subdomain is: ${(req.subdomains)}`);
-  io.on("connection", function (data) {
-    console.log(`Made socket connection, socket is ${data.id}`);
-    console.log(`Hostname: ${INChostname}`);
-    io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.localhost', '')});
-    // io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.myservvio.com', '')});
-  });
-  next()
+
+  //return promise function 
+    //inside function get store front content for this store using storename
+  //.then(()=>{emit data below})
+  //show data on react page initially as words to test working
+  const data = {"store_name": storename}
+  
+    //firebase
+  console.log("Store name being sent to firebase is: " + storename)
+  axios.post("https://us-central1-dashtest-7cb07.cloudfunctions.net/getStoreContent", data ).then((storeContent)=>{
+    //console.log("store content is: " + storeContent);
+
+    console.log("store content data is: " + JSON.stringify(storeContent.data))
+    store_front_content = storeContent.data
+    io.on("connection", function (socket) {
+      console.log(`Made socket connection, socket is ${socket.id}`);
+      console.log(`Hostname: ${INChostname}`);
+      io.emit("subdomain", {"socket id": socket.id, "hostname": INChostname.replace('.localhost', ''), "details": store_front_content});
+      // io.emit("subdomain", {"socket id": socket.id, "hostname": INChostname.replace('.myservvio.com', ''), "details": store_front_content});
+      setTimeout(() => socket.disconnect(true), 1000);
+
+    });
+    next()
+  }, (error)=>{
+    console.log("error occurred: " + error)
+  })
+  
 })
+
+// app.use('/bookings', function(req, res, next){
+//   INChostname = req.hostname
+//   storename = req.hostname.replace('.localhost', '')
+
+//   console.log(`hello here works in middleware req.data: ${req}, hostname: ${req.hostname}, subdomain is: ${(req.subdomains)}`);
+
+//   //return promise function 
+//     //inside function get bookings data for this store using storename
+//   //.then(()=>{emit data below})
+
+//   io.on("connection", function (data) {
+//     console.log(`Made socket connection, socket is ${data.id}`);
+//     console.log(`Hostname: ${INChostname}`);
+//     io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.localhost', ''), "bookings_content": booking_page_content});
+//     // io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.myservvio.com', '')});
+//   });
+//   next()
+// })
+
+
+// app.use('/account', function(req, res, next){
+//   INChostname = req.hostname
+//   storename = req.hostname.replace('.localhost', '')
+
+//   console.log(`hello here works in middleware req.data: ${req}, hostname: ${req.hostname}, subdomain is: ${(req.subdomains)}`);
+
+//   //return promise function 
+//     //inside function get bookings data for this store using storename
+//   //.then(()=>{emit data below})
+
+//   io.on("connection", function (data) {
+//     console.log(`Made socket connection, socket is ${data.id}`);
+//     console.log(`Hostname: ${INChostname}`);
+//     io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.localhost', ''), "account_details": account_details});
+//     // io.emit("subdomain", {"socket id": data.id, "hostname": INChostname.replace('.myservvio.com', '')});
+//   });
+//   next()
+// })
+
 
 app.use(express.static(path.join(__dirname, "build")))
 
--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ const { io } = require("socket.io-client");
 
 
 var subdomain;
+var storeContent;
+var storeTitle;
+var storeDescription;
 const port = process.env.PORT || 5000;
 
 function App() {
@@ -12,10 +15,15 @@ function App() {
   socket.connect(`http://localhost:${port}`);
   const [storename, setStorename] = useState('');
   socket.on("subdomain", (data)=>{
+    socket.disconnect();
     subdomain = data.hostname
+    storeContent = data["details"]
+    console.log("store details are: " + JSON.stringify(data["details"]))
+    storeTitle = data.details["page_title"];
+    storeDescription = data.details["description"]
     setStorename(data.hostname) 
     console.log(`data sent is ${JSON.stringify(data)}`);
-    socket.disconnect();
+    
   })
 
   // socket.on("connection", function (socket) {
@@ -25,6 +33,10 @@ function App() {
     <div className="App">
       <h1> Welcome to {subdomain} store </h1>
       <h2> Store name is: {storename} </h2>
+      <br></br>
+      <h2>Store Title: {storeTitle}</h2>
+      <h2>Store Description: {storeDescription}</h2>
+
       <p>
         Hi Test App works
       </p>
