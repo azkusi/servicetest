630f0956a4aaeb86e2c8309e92139b4bf683c873
azkusi <azkusi1@gmail.com>
2021-12-22 02:10:50 +0000

    send client message to firebase

--- client/src/store_pages/Messages.js
+++ client/src/store_pages/Messages.js
@@ -1,6 +1,6 @@
 import "../styles.css";
 import React, { useRef, useState } from "react";
-// import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form, Button, Card } from "react-bootstrap";
 import Alert from '@mui/material/Alert';
 
 import {app} from '../firebase';
@@ -23,61 +23,74 @@ function Messages ({ serviceContent }) {
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
   const [success, setSuccess] = useState(false)
+  const nameRef = useRef()
+  const messageRef = useRef()
+  const emailRef = useRef()
   // const [serviceProviderData, setServiceProviderData] = useState(serviceContent);
   // const [value, setValue] = useState()
   // const onInput = ({target:{value}}) => setValue(value)
  
  
-  function onFormSubmit (e){
-    setSuccess(true)
+  async function onFormSubmit (e){
     e.preventDefault();
-    console.log("Name: " + name)
-    console.log("Email: " + email)
-    console.log("Message: " + message)
-  }
+    const nameSent = nameRef.current.value;
+    const emailSent = emailRef.current.value
+    const messageSent = messageRef.current.value
+    console.log("Name: " + nameSent)
+    console.log("Email: " + emailSent)
+    console.log("Message: " + messageSent)
+    setSuccess(true)
+    try{
+      const convoref = db.collection('serviceProviders').doc(serviceContent.service_provider_name).collection('conversations').doc()
+      const convorefID = convoref.id
+      console.log("convorefID is: " + convorefID)
+      await convoref.set({"last_message_sent": "heyheyhey"})
+      await db.collection('serviceProviders').doc(serviceContent.service_provider_name).collection('conversations').doc(convorefID).collection('messages').add({"message": messageSent, "name": nameSent, "email": emailSent})
+
+    }
+    catch(err){
+      console.log("error is: " + err)
+    }
+}
 
 
     return (
       <>
       <div>
-        <h2>Hi, {serviceContent.service_provider_name}</h2> 
+        <h2>Messages</h2> 
       </div>
       <div>
-      {!success && <form onSubmit={onFormSubmit}>
-        
+      {!success && 
+      <Card>
+      <Card.Body>
+        <Form onSubmit={onFormSubmit}>
+          <Form.Group id="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} placeholder="name" required />
+          </Form.Group>
 
-        <input
-          placeholder="Name" 
-          value={name}
-          onChange={(e) => {setName(e.target.value)
-          console.log("Name is: " + name)}}
-        />
-
-
-        <input
-          label="email" 
-          value={email}
-          onChange={(e) => {setEmail(e.target.value)
-          console.log("Email is: " + email)}}
-        />
-
-          <textarea
-            type="text" 
-            placeholder="Type message"
-            value={message}
-            onChange={(e) => {setMessage(e.target.value)
-            console.log("Message is: " + message)}}
-          />
-        <input type="submit" value="Send Message" />
-      </form> }
+          <Form.Group id="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" ref={emailRef} placeholder="email" required />
+          </Form.Group>
+          
+          <Form.Group id="message">
+            <Form.Label>Message</Form.Label>
+            <Form.Control type="text" ref={messageRef} placeholder="Type message" required />
+          </Form.Group>
+          <Button type="submit">Send Message</Button>
+        </Form>
+        </Card.Body>
+        </Card>}
 
 
-      {success && <h2>Successfully sent</h2>}
+      {/* {success && <h2>Successfully sent</h2>} */}
 
-      {/* {success && <Alert severity="success">
+      {success && <Alert severity="success">
         Your message was successfully sent to {serviceContent.service_content.page_title}
+        <br/>
         We will send you an email when {serviceContent.service_content.page_title} responds to your message.
-      </Alert> }  */}
+      </Alert> }  
 
       </div>
       </>
